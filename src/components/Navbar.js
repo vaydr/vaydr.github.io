344a6ac1d508640598b23fe6145905c5d0b383c9
vaydr <87049538+vaydr@users.noreply.github.com>
2024-06-07 18:10:52 -0400

    updated skeleton to compile correctly

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,31 @@
-// src/components/Navbar.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-brand">MyWebsite</div>
-      <div className="navbar-links">
-        <a href="#home">Home</a>
-        <a href="#about">About</a>
-        <a href="#services">Services</a>
-        <a href="#contact">Contact</a>
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          vayd
+        </Link>
+        <ul className="nav-menu">
+          <li className="nav-item">
+            <Link to="/" className="nav-links">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about" className="nav-links">About</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/cv" className="nav-links">CV</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/projects" className="nav-links">Projects</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/contact" className="nav-links">Contact</Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
---
+++ src/pages/CV.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CV = () => {
+  return (
+    <div>
+      <h1>Curriculum Vitae</h1>
+      <p>This is the CV page.</p>
+    </div>
+  );
+};
+
+export default CV;
