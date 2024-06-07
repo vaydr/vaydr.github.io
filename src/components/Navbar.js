2d59ba728971c6841fe4e6f09914f9b96ec9ee41
vaydr <87049538+vaydr@users.noreply.github.com>
2024-06-07 17:35:37 -0400

    added basic navbar skeleton

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+// src/components/Navbar.js
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">MyWebsite</div>
+      <div className="navbar-links">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#services">Services</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
