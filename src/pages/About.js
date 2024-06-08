7559b8010e3b4914a3297f43dc085a212a2fddca
vaydr <87049538+vaydr@users.noreply.github.com>
2024-06-08 16:36:32 -0400

    ready to deploy first version of about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,13 +10,13 @@ const About = () => {
         Hi, I'm <span className="Highlight">Vayd</span>. I was born in New Jersey and moved to Mississippi during my childhood. I graduated with a degree in AI and Math from MIT and am currently a software engineer in the advertising department at TikTok.
       </p>
       <p className="Paragraph">
-        My academic journey includes courses in distributed algorithms, sublinear algorithms, and GANs for privacy-focused content. Both of my parents are professors—my father in Computer Science and my mother in Biology—at a university in Mississippi. Their professions deeply inspired my passion for the intersection of machine learning and biology.
+        My academic journey includes courses in distributed algorithms, sublinear algorithms, and GANs for privacy-focused content. Both of my parents are professors—my father in Computer Science and my mother in Biology—at Mississippi State University. Their professions deeply inspired my passion for the intersection of machine learning and biology.
       </p>
       <p className="Paragraph">
         I am excited about the potential of technology to advance our understanding of biological systems, a field that holds promise for groundbreaking discoveries.
       </p>
       <p className="Paragraph">
-        When I'm not immersed in my professional interests, I enjoy working out and exploring Boston.
+        In my spare time, I enjoy working out, playing VALORANT, and exploring Boston.
       </p>
     </div>
   );
