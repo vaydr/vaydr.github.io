cfd95d92acafe8718e5bb8c61b2aa247d908ca89
vaydr <vramkumar369@gmail.com>
2024-06-15 15:53:55 -0700

    starting work on about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,23 +4,35 @@ import './About.css';
 const About = () => {
   return (
     <div className="AboutContainer">
-      <h1 className="Title">About Me</h1>
-      <h2 className="Subtitle">Welcome to my world!</h2>
-      <p className="Paragraph">
-        Hi, I'm <span className="Highlight">Vayd</span>. I was born in New Jersey and moved to Mississippi during my childhood. I graduated with a degree in AI and Math from MIT and am currently a software engineer in the advertising department at TikTok.
-      </p>
-      <p className="Paragraph">
-        My academic journey includes courses in distributed algorithms, sublinear algorithms, and GANs for privacy-focused content. Both of my parents are professors—my father in Computer Science and my mother in Biology—at Mississippi State University. Their professions deeply inspired my passion for the intersection of machine learning and biology.
-      </p>
-      <p className="Paragraph">
-        I am excited about the potential of technology to advance our understanding of biological systems, a field that holds promise for groundbreaking discoveries.
-      </p>
-      <p className="Paragraph">
-        In my spare time, I enjoy working out, playing VALORANT, and exploring Boston.
-      </p>
+      <div className="FastFactsCard">
+        <h2 className="CardTitle">Fast Facts</h2>
+        <ul className="FastFactsList">
+          <li>Born in New Jersey</li>
+          <li>Raised in Mississippi</li>
+          <li>MIT grad in AI and Math</li>
+          <li>Software Engineer at TikTok</li>
+          <li>Parents are professors at MSU</li>
+        </ul>
+      </div>
+      {/* 
+      <div className="AboutContent">
+        <h1 className="BigBoldText">About Me</h1>
+        <p className="Paragraph">
+          Hi, I'm <span className="Highlight">Vayd</span>. Born in New Jersey, raised in Mississippi. MIT grad in AI and Math. Now, a software engineer at TikTok's advertising department.
+        </p>
+        <p className="Paragraph">
+          My academic journey? Distributed algorithms, sublinear algorithms, and GANs for privacy-focused content. My parents, both professors at Mississippi State University, fueled my passion for the intersection of machine learning and biology.
+        </p>
+        <p className="Paragraph">
+          I'm all about leveraging technology to unlock the mysteries of biological systems. The future is bright with groundbreaking discoveries.
+        </p>
+        <p className="Paragraph">
+          Outside of work, you'll find me working out, playing VALORANT, or exploring Boston.
+        </p>
+      </div>
+      */}
     </div>
   );
 };
 
 export default About;
-
