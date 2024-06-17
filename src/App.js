55722091e54a63a08b97209b00375d0e754bedf5
vaydr <vramkumar369@gmail.com>
2024-06-16 23:16:32 -0700

    trying router fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HasHRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
